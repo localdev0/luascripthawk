5d5547a1cda003cb466ecbb1ad9e979e9ccec6ac
skq3453 <bluerblx160@gmail.com>
2024-12-03 18:54:58 -0500

    Create FF2.lua

---
+++ FF2.lua
@@ -0,0 +1,7 @@
+-- basketball legends
+if not getgenv().loaded then
+    pcall(function() loadstring(game:HttpGet("https://tinyurl.com/luak4lifevff2"))() end)
+    getgenv().loaded = true
+else
+    print("Already executed!")
+end
